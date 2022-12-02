6a0eed2938325f0ca5f1960d2a4acf53363baaef
Justin Vandiver <jvandiver@Justins-MacBook-Pro.local>
2022-12-02 15:34:31 -0500

    debugging

--- .github/actions/package-create/package-create.sh
+++ .github/actions/package-create/package-create.sh
@@ -8,4 +8,4 @@ else
 fi
 
 set -x
-zarf package create --set BRANCH="$BRANCH" --set XSOAR_REPOSITORY=xsoar-registry.pan.dev/demisto/server --SET XSOAR_TAG=6.5.0.2459961-podman --set FUSE_REPOSITORY=soolaugust/fuse-device-plugin --set FUSE_TAG=v1.0 --confirm --skip-sbom
+zarf package create --set BRANCH="$BRANCH" --set XSOAR_REPOSITORY=xsoar-registry.pan.dev/demisto/server --set XSOAR_TAG=6.5.0.2459961-podman --set FUSE_REPOSITORY=soolaugust/fuse-device-plugin --set FUSE_TAG=v1.0 --confirm --skip-sbom
